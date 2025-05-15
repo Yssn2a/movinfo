6f30165325de81d4ec768d16b619408a512c1774
Yssn2a <125408999+Yssn2a@users.noreply.github.com>
2025-05-16 00:29:54 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ document.addEventListener("DOMContentLoaded", function () {
   if (currentPage === "index.html") {
    
     document.querySelector('[data-page="search"]').addEventListener('click', function () {
-      window.location.href = 'yssn2a.github.io/movinfo/Search/search.html';
+      window.location.href = '/movinfo/Search/search.html';
     });
 
     
     document.querySelector('[data-page="latest"]').addEventListener('click', function () {
-      window.location.href = 'yssn2a.github.io/movinfo/Popular/popular.html';
+      window.location.href = '/movinfo/Popular/popular.html';
     });
   }
 });
